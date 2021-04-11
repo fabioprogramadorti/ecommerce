10fdf6ccea51a7c390f208ca610b99447b67ba2b
fabioprogramadorti <fabioprogramadorti@gmail.com>
2021-04-11 18:47:15 -0300

    Change url to real api

--- lib/products.ts
+++ lib/products.ts
@@ -1,12 +1,12 @@
 export async function getProductsData() {
 
-  const response = await fetch('https://ecommerce.free.beeceptor.com/product/list')
+  const response = await fetch('http://localhost/product/list')
   const data = await response.json()
   return data.products
 }
 export async function getProductsDetail(id) {
 
-  const response = await fetch(`https://ecommerce.free.beeceptor.com/product/${id}`)
+  const response = await fetch(`http://localhost/product/?id=${id}`)
   const data = await response.json()
   return data.product
 }
